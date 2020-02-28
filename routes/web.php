7400b6a00fb67a32e063b6204907d50cc7704600
arif <bickkuarif@gmail.com>
2020-02-28 22:07:29 +0530

    updated code test

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,8 @@ Route::get('/test', function () {
 	//     }
 	// }
 	$path = public_path('template1');
-	$process = new Process('terraform init -input=false');
+	//$process = new Process('terraform init -input=false');
+	$process = new Process('ls -lrtha');
 	$process->setTimeout(3600);
     $process->setWorkingDirectory($path);
     $process->run();
